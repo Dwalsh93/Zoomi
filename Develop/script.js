56541ee5c4b37efb7a77c2d4f132654a3e8313ee
dillon walsh <dillonwalsh93@gmail.com>
2021-05-09 19:24:18 -0400

    still cant get final password to generate

--- Develop/script.js
+++ Develop/script.js
@@ -51,14 +51,18 @@ function writePassword() {
 
 }
 
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialChar = ["±","!","@","#","$","%","^","&","*","(",")","_","+","-","=","§","£","™","¡","¢","∞","§","¶","•","ª","º","–","≠"];
+var numeric = ["1","2","3","4","5","6","7","8","9","0"];
 
 function randomGen() {
   check();
   var passwordText = document.querySelector("#password");
 
-  var finalpassword = "";
+  var password = "";
   for (var i= 0; i < charlength; i++) {
-    finalpassword += passwordCheck[Math.floor(Math.random() * passwordCheck.length)];
+    password += password[Math.floor(Math.random() * password.length)];
   }
   
   
