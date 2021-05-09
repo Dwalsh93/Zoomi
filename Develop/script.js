95403612b50870c73d1f2a8d1b9305efaabededf
dillon walsh <dillonwalsh93@gmail.com>
2021-05-08 20:09:59 -0400

    trying new js for alerts

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,28 @@
+console.log("first");
+
 // Assignment code here
 var charlength = 0; 
-var lowercase = false;
-var uppercase = false;
-var specialchar = false;
-var numeric = false;
-var nameprompt = prompt("Please enter your name")
-console.log (nameprompt);
-
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var specialchar = ["±!@#$%^&*()_+-=§£™¡¢∞§¶•ªº–≠"];
+var numeric = ["1234567890"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  // document.querySelector("#generate");
+  var charlength = prompt("How many characters would you like your password to contain?")
+  console.log (nameprompt);
   passwordText.value = password;
+  
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
