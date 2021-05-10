2b09fe49926506a57cd335d1ef6a2389a0fcc93f
dillon walsh <dillonwalsh93@gmail.com>
2021-05-10 18:55:39 -0400

    fixed generation issue

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,12 @@
-console.log("first");
+// console.log("first");
 
-// Variable Outline
-var charlength = 0;
-var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
-var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var specialchar = ["±!@#$%^&*()_+-=§£™¡¢∞§¶•ªº–≠"];
-var numeric = ["1234567890"];
-var finalpassword = "";
+// // Variable Outline
+// var charlength = 0;
+// var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+// var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+// var specialchar = ["±!@#$%^&*()_+-=§£™¡¢∞§¶•ªº–≠"];
+// var numeric = ["1234567890"];
+// var finalpassword = "";
 
 
 // Get references to the #generate element
@@ -15,33 +15,46 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 // Character Length Section Begin
 
-function writePassword() {
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialChar = ["±","!","@","#","$","%","^","&","*","(",")","_","+","-","=","§","£","™","¡","¢","∞","§","¶","•","ª","º","–","≠"];
+var numeric = ["1","2","3","4","5","6","7","8","9","0"];
+
+var charSet = [];
+
+var finalpassword = ""
+
+function randomGen() {
   var passwordText = document.querySelector("#password");
+
   var charlength = prompt("How many characters would you like your password to contain?")
-  passwordText.value = password;
 
   if (charlength <= 7 || charlength >= 129) {
     alert("Password length must be between 8-128 characters Try again");
-    passLength = parseInt(charlength);
-    return charlengthselect();
+    //passLength = parseInt(charlength);
+    return; //charlengthselect();
 
   }
   lowercaseselect = confirm("Would you like to use lowercase letters?");
   console.log (lowercaseselect)
   if (lowercaseselect) {
-    finalpassword += lowercase
+    charSet = charSet.concat(lowerCase);
+    //finalpassword += lowercase
   }
   uppercaseselect = confirm("Would you like to use uppercase letters?");
   if (uppercaseselect) {
-    finalpassword += uppercase
+    charSet = charSet.concat(upperCase);
+    // finalpassword += uppercase
   }
   specialcharselect = confirm("Would you like to use special characters?");
   if (specialcharselect) {
-    finalpassword += specialchar
+    charSet = charSet.concat(specialChar);
+    // finalpassword += specialchar
   }
   numericselect = confirm("Would you like to use numbers?");
   if (numericselect) {
-    finalpassword += numeric
+    charSet = charSet.concat(numeric);
+    // finalpassword += numeric
   }
   console.log ({lowercaseselect, numericselect, specialcharselect, uppercaseselect})
   if (!numericselect && !specialcharselect && !uppercaseselect && !lowercaseselect) {
@@ -50,25 +63,12 @@ function writePassword() {
 
   }
 
-}
-
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var specialChar = ["±","!","@","#","$","%","^","&","*","(",")","_","+","-","=","§","£","™","¡","¢","∞","§","¶","•","ª","º","–","≠"];
-var numeric = ["1","2","3","4","5","6","7","8","9","0"];
-
-var finalpassword = ""
-
-function randomGen() {
-  var passwordText = document.querySelector("#password");
-
-  var finalpassword = "";
   for (var i= 0; i < charlength; i++) {
-    finalpassword += password[Math.floor(Math.random() * password.length)];
+    finalpassword += charSet[Math.floor(Math.random() * charSet.length)];
   }
   
   passwordText.value = finalpassword;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", randomGen);
